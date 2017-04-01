680ea05c061bc1525b83e3b2785bbf9b647df2c8
Dan Cancelmo <dcancelm@u.rochester.edu>
2017-04-01 19:25:14 -0400

    myAppend start

--- Racket_Functions.rkt
+++ Racket_Functions.rkt
@@ -1,4 +1,6 @@
 #lang racket
 
 (define (myAppend list1 list2)
-  (cons list1 list2))
+  ;(let (newList [list first(list1) rest(list1) first(list2) rest(list2)]))
+  ;newList)
+  (list (car list1) (cdr list1) (car list2) (cdr list2)))
