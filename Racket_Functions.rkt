c44ccf49fade654a9b7bc78c0132b80241978679
Dan Cancelmo <dcancelm@u.rochester.edu>
2017-04-02 18:20:58 -0400

    List functions done

--- Racket_Functions.rkt
+++ Racket_Functions.rkt
@@ -1,6 +1,34 @@
 #lang racket
 
-(define (myAppend list1 list2)
-  ;(let (newList [list first(list1) rest(list1) first(list2) rest(list2)]))
-  ;newList)
-  (list (car list1) (cdr list1) (car list2) (cdr list2)))
+;List functions
+
+(define (myappendH firstElem restElem)
+  (if (empty? firstElem)
+      restElem
+      (cons (first firstElem) (myappendH (rest firstElem) restElem))
+      )
+  )
+(define (myappend list1 list2)
+  (myappendH list1 list2)
+  )
+
+(define (indexofH item listP counter)
+  (cond
+    [(empty? listP) #f]
+    [(eq? item (car listP)) counter]
+    [else (indexofH item (cdr listP) (+ counter 1))]
+    )
+  )
+(define (indexof item listP)
+  (indexofH item listP 0)
+  )
+
+(define (addtoendH listP item)
+  (cond
+    [(empty? listP) item]
+    [else (cons (car listP)(addtoendH (cdr listP) item))]
+    )
+  )
+(define (addtoend listP item)
+  (addtoendH listP (cons item '()))
+  )
